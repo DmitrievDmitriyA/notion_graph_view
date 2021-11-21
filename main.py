264348019abf0fcfca667c0748f587bb06d8d717
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2021-11-21 14:30:07 +0300

    Managed to draw

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import json
 from notion_requests import Client
+import networkx as nx
 
 
-base_url = "https://api.notion.com/v1/databases/"
-api_version = "2021-08-16"
+def decode(x):
+    return [i for i in x]
 
 
 def load_config():
     with open("config.json", "r") as read_file:
         return json.load(read_file)
 
-def main():
-    config = load_config()
+
+def obtain_pages(config):
     notion_key = config["key"]
     notion_database_id = config["database_id"]
 
@@ -28,12 +29,36 @@ def main():
     }
 
     pages = []
+
     # query a database
     # it returns a generator object since this api endpoint supports pagination
     for response in notion.databases.query(**query):
         pages.extend(response['results'])
 
-    print(len(pages))
+    return pages
+
+
+def main():
+    config = load_config()
+    pages = obtain_pages(config)
+
+    nodes = []
+    edges = []
+
+    for page in pages:
+        # add the page to nodes
+        nodes.append(page["id"])
+
+        # check links to previous cards and add edges
+        # care only about previous links cause next links only duplicate them
+        for node in page["properties"]["Previous card"]["relation"]:
+            e = (page["id"], node["id"])
+            edges.append(e)
+
+    graph = nx.Graph()
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+    nx.write_graphml(graph, "./zettelkasten.graphml")
 
 
 if __name__ == "__main__":
