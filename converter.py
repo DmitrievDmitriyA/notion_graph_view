6630f31c319ad11d13f8b5723afbfb7295b7879f
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2022-02-20 19:19:21 +0300

    Draw edges in a new way

--- converter.py
+++ converter.py
@@ -1,44 +1,33 @@
 
 
 def notion_to_yEd(relations_per_database, pages_per_database):
+
     nodes_per_database = []
     edges_per_database = []
 
-    for pages in pages_per_database:
-        nodes = []
-        for page in pages:
-            # obtain id and name of every page to form a node
-            nodes.append({
-                "id": page["id"],
-                "name": page["properties"]["Name"]["title"][0]["text"]["content"]
-            })
+    for index, pages in enumerate(pages_per_database):
+        relations = relations_per_database[index]
+        nodes, edges = _convert_database(relations, pages)
         nodes_per_database.append(nodes)
+        edges_per_database.append(edges)
 
-    # # extract names from tags database
-    # for page in tags_pages:
-    #     tags.append({
-    #         "id": page["id"],
-    #         "name": page["properties"]["Name"]["title"][0]["text"]["content"]
-    #     })
+    return nodes_per_database, edges_per_database
 
-    # for page in zet_pages:
-    #     # add the page to nodes
-    #     nodes.append({
-    #         "id": page["id"],
-    #         "type": page["properties"]["Type"]["select"]["name"],
-    #         "content": page["properties"]["Name"]["title"][0]["text"]["content"]
-    #         })
 
-        # # extract tags edges from the page
-        # for tag in page["properties"]["Tags"]["relation"]:
-        #     e = (page["id"], tag["id"])
-        #     tags_edges.append(e)
+def _convert_database(relations, pages):
+    nodes = []
+    edges = []
 
-        # # extract edges from the page
-        # # check links to previous cards and add edges
-        # # care only about previous links cause next links only duplicate them
-        # for node in page["properties"]["Previous card"]["relation"]:
-        #     e = (page["id"], node["id"])
-        #     edges.append(e)
+    for page in pages:
+        # obtain id and name of every page to form a node
+        nodes.append({
+            "id": page["id"],
+            "name": page["properties"]["Name"]["title"][0]["text"]["content"]
+        })
 
-    return (nodes_per_database, edges_per_database)
+        for id, name in relations.items():
+            for relation in page["properties"][name]["relation"]:
+                e = (page["id"], relation["id"])
+                edges.append(e)
+
+    return (nodes, edges)
