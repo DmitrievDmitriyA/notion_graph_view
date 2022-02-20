cdabae797de3d8123bc7da0026799efa0798a88a
Dmitriy Dmitriev <dmitriev.dmitry.a@yandex.ru>
2022-02-20 12:38:40 +0300

    Adapted secrets to read new config file

--- secrets.py
+++ secrets.py
@@ -4,4 +4,4 @@ import json
 def load_config():
     with open("config.json", "r") as read_file:
         config = json.load(read_file)
-        return (config["token"], config["zet_database_id"], config["tags_database_id"])
+        return (config["token"], config["database_ids"])
